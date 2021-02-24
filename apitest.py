7d24a38bad3623d22fd2fb837ee652db1521df0f
florianlandras <61586196+florianlandras@users.noreply.github.com>
2021-02-24 21:28:52 +0100

    desoler oups

--- apitest.py
+++ apitest.py
@@ -3,23 +3,32 @@ from flask import Flask, request, render_template, session, redirect
 import requests
 import json
 import pandas as pd
-
+"""
 data = requests.get("https://api.battlemetrics.com/servers/10377404")
 data = data.json()
 
 df = pd.json_normalize(data)
 df
+"""
 
 
-
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, template_folder="templates")
 app.config["DEBUG"] = True
 
 
 @app.route('/', methods=["POST",'GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
 @app.route('/api', methods=["POST",'GET'])
-def html_table():
-    return render_template('simple.html',  tables=[df.to_html()], titles=df.columns.values)
+
+
+def summary():
+    data = requests.get("https://api.battlemetrics.com/servers/10377404")
+    data = data.json()
+    response = app.response_class(
+        response=json.dumps(data),
+        mimetype='application/json'
+    )
+    return response
 app.run()
