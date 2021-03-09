e73c4dada8fc96010f4b574cb07d345d5e9cc26b
florianlandras <61586196+florianlandras@users.noreply.github.com>
2021-03-09 21:48:49 +0100

    docker update

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# For more information, please refer to https://aka.ms/vscode-docker-python
+FROM python:3.8-slim-buster
+
+EXPOSE 5000:5000
+
+RUN apt-get update -y && \
+    apt-get install -y python-pip python-dev
+
+# We copy just the requirements.txt first to leverage Docker cache
+COPY ./requirements.txt /valheimApi/requirements.txt
+
+WORKDIR /valheimApi
+
+RUN pip install -r requirements.txt
+
+COPY . /valheimApi
+
+ENTRYPOINT [ "python" ]
+
+CMD [ "main.py" ]
--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 import flask
 from flask_cors import CORS # https://flask-cors.readthedocs.io/en/latest/
+import requests
+import pandas as pd
 from serverData import playerNumber
 from serverData import server
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect
 
 app = flask.Flask(__name__, template_folder="templates")
 app.config["DEBUG"] = True
 CORS(app)
+
 
 @app.route('/', methods=["POST",'GET'])
 def index():
     return render_template('index.html')
 
 @app.route('/api', methods=["POST",'GET'])
-def summary():  
+def summary():
     return server().to_json(orient="table")
 
 @app.route('/graph', methods=['GET'])
 def graph():
-    
     startDate = request.args.get('startDate')
     endDate = request.args.get('endDate')
-    
     if startDate is None :
         startDate = "2021-02-27"
     if endDate is None :
         endDate = "2021-03-05"
-
     return playerNumber(startDate,endDate).to_json(orient="table")
 
-app.run()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
